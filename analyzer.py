822880dbdf73845147d4a5faaa610964da888c6f
anmolk11 <akcp.cpp@gmail.com>
2023-04-19 16:28:37 +0530

    added preprocessing

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,35 @@
-import nltk
+import numpy as np
+import pandas as pd
 from nltk.sentiment import SentimentIntensityAnalyzer
+
+from clean import preprocess_text
 
 def analyze(text):
     sia = SentimentIntensityAnalyzer()
     scores = sia.polarity_scores(text)
-    return scores
+    scores = scores["compound"]
+    score_out_of_5 = (scores + 1) * 2.5
+    return score_out_of_5
 
 if __name__ == "__main__":
-    pass
+    actual_stars = []
+    nlp_score = []
+    df = pd.read_csv("reviews.csv")
+    
+    for k,v in df.iterrows():
+        text = v['review']
+        print(text)
+        text = preprocess_text(text)
+        try:
+            nlp_scr = analyze(text)
+        except:
+            pass
+        stars = v['stars']
+        print(text)
+        print(nlp_scr,stars)
+        actual_stars.append(stars)
+        nlp_score.append(nlp_scr)
+        print("\n-------------------\n")
+
+    # corr_coef = np.corrcoef(actual_stars, nlp_score)[0, 1]
+    # print(corr_coef)
--- clean.py
+++ clean.py
@@ -1,33 +1,40 @@
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+# from spellchecker import SpellChecker
 
-stop_words = set(stopwords.words('english'))
-
-def remove_stopwords(text):
+def preprocess_text(text):
+    # Tokenize the text
     words = nltk.word_tokenize(text)
-    words = [word.lower() for word in words if word.lower() not in stop_words]
-    return ' '.join(words)
+    
+    # Convert to lowercase
+    words = [word.lower() for word in words]
+    
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in words if word not in stop_words]
+    
+    # Lemmatize the words
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    
+    # Remove special characters and numbers
+    words = [re.sub(r'[^a-zA-Z0-9]', '', word) for word in words]
+    
+    # Spell correction
+    # spell = SpellChecker()
+    # words = [spell.correction(word) for word in words]
+    
+    # Join the words back into a string
+    text = ' '.join(words)
+    
+    return text
 
-
-def remove_emojis(text):
-    emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  
-        u"\U0001F300-\U0001F5FF"  
-        u"\U0001F680-\U0001F6FF"  
-        u"\U0001F1E0-\U0001F1FF"  
-        u"\U00002702-\U000027B0"  
-        u"\U000024C2-\U0001F251"  
-        u"\U0001F900-\U0001F9FF"  
-        u"\U0001F1F2-\U0001F1F4"  
-        u"\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF\U00002702-\U000027B0\U000024C2-\U0001F251\U0001F900-\U0001F9FF"  # new emojis
-                           "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', text)
 
 
 if __name__ == "__main__":
     text = "I am anmol khandelwal 😂"
-    text = remove_emojis(text)
-    text = remove_stopwords(text)
+    text = preprocess_text(text)
     print(text)
     
